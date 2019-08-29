d67eaf230f4e46033812369517ecc9f24e782909
edward sams <40345707+ewsams@users.noreply.github.com>
2019-08-28 22:44:16 -0400

    Add files via upload

--- IBluetooth.java
+++ IBluetooth.java
@@ -1,6 +1,6 @@
 package com.revature.ewsoop;
 
 public interface IBluetooth {
-	void turnOnBlueTooth();
+	void turnOnBlueTooth() throws Exception;
 	void listenToMusic(int volume);
 }
--- Truck.java
+++ Truck.java
@@ -4,32 +4,12 @@ public class Truck extends Automobile  {
 	
 		private static String type = "Truck";
 		public static int truck_population = 0 ;
+		
 		public Truck(String make, int year,String color, boolean running) {
 			super(make,year,color,running);
 			truck_population++;
 		}
 
-		//overloading our constructor for the Truck class
-				// placing default values for method calls of differing parameters 
-				public Truck(String make,int year , String color) {
-					this(make,year,color,true);	
-					truck_population++;
-				}
-
-				public Truck(String make,int year) {
-					this(make,year,"blue");	
-					truck_population++;
-				}
-				
-				public Truck(String make) {
-					this(make,0);	
-					truck_population++;
-				}
-				
-				public Truck() {
-					this("Ford");
-					truck_population++;
-				}
 		public static void printPopulation() {
 			if(truck_population == 0) {
 				System.out.println("There are no trucks available ");
@@ -44,26 +24,40 @@ public class Truck extends Automobile  {
 			System.out.println(this.getMake() + "placed in park mode");
 
 		}
-		//overloading
-		public void fillGas(int cost) {
-			System.out.println(type + " filling with gas");
-			System.out.println("please pay:" + cost);
-			
+		
+		//method overloading
+		public void fillGas(String gas) {
+			try {
+			if(gas=="gradeB") {
+				System.out.println("This grade is valid. Thanks for using gradeB");
+				System.out.println(type + " filling up with gas");
+			}
+			else {
+				System.out.println("Please only use gasGrade --> gradeB");
+			}
+		} catch(Exception e) {
+			System.out.println(e + "please include --> classB gasoline only");
+			}
 		}
 
-		public void turnOnBlueTooth() {
+		public void turnOnBlueTooth() throws Exception {
+			if(blueTooth) {
+				System.out.println("Google Voice is avaialable....");
+			}
 			
+			else if(blueTooth & engineOn) {
+				System.out.println("Google Voice is avaialable....");
+			}
+			
+			else {
+				throw new Exception("Please turn on Blue Tooth While Driving...");
+			}
+
 		}
 
 		@Override
 		public void fillGas() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void autoDrive() {
-			// TODO Auto-generated method stub
-			
-		}
-
+			System.out.println("Currently at a full Tank of Gas");
+		}			
 }
+
