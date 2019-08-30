169cbecb10001c5dd54f53bd077406797bd9ddd0
edward sams <40345707+ewsams@users.noreply.github.com>
2019-08-30 07:15:27 -0400

    Add files via upload

--- AutomobileDriver.java
+++ AutomobileDriver.java
@@ -8,11 +8,11 @@ public class AutomobileDriver  {
 		Truck F150 = new Truck("Ford", 2007, "blue", true);
 		SportsCar Mustang = new SportsCar("Ford", 2019, "red/black", true);
 		SportsCar BMW3 = new SportsCar("BMW", 2017, "White", true);
-		SportsCar BMW5 = new SportsCar("BMW", 2001, "Grey", true);
+		SportsCar BMW5 = new SportsCar("BMW", 1999, "Grey", true);
+		SportsCar GT = new SportsCar("Ford", 2001, "blue/white", true);
 		Truck Tundra = new Truck("Toyota", 1993, "blue", false);
 		MiniVan Sienna = new MiniVan("Ford", 2002, "green", false);
 		Truck F350 = new Truck("Ford",2018,"black",true);
-		SportsCar GT = new SportsCar("Ford", 2001, "blue/white", true);
 		//methods used:
 		Mustang.startEngine(true);
 		Mustang.revEngine();
@@ -28,14 +28,14 @@ public class AutomobileDriver  {
 		F350.startEngine();
 		System.out.println("************************************************************************************");
 		System.out.println("-----------------Now Lets take a look at our unsorted Array List------------------");
-		for(Automobile objectinAutomobileList: Automobile.autoList) {
-			System.out.println(objectinAutomobileList);
+		for(Automobile objectInAutomobileList: Automobile.autoList) {
+			System.out.println(objectInAutomobileList);
 		}
 		System.out.println("************************************************************************************");
 		System.out.println("---------------------Now are Sorted ArrayList by Year in descending order---------");
 		Collections.sort(Automobile.autoList, Automobile.yearComparator);
-		for(Automobile objectinAutomobileList: Automobile.autoList) {
-			System.out.println(objectinAutomobileList);
+		for(Automobile objectInAutomobileList: Automobile.autoList) {
+			System.out.println(objectInAutomobileList);
 		}
 		
 	}
