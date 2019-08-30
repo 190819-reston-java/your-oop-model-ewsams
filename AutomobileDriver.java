b3f2b3e78abcd63fdfb13b5dbc844392a28af94b
edward sams <40345707+ewsams@users.noreply.github.com>
2019-08-29 22:34:00 -0400

    Add files via upload

--- AutomobileDriver.java
+++ AutomobileDriver.java
@@ -1,25 +1,38 @@
 package com.revature.ewsoop;
+import java.util.Collections;
 
 public class AutomobileDriver  {
 
 	public static void main(String[] args) throws Exception {
-		SportsCar Mustang = new SportsCar("Ford", 2019, "red", true);
-		Truck F150 = new Truck("Ford", 2019, "blue", true);
+		SportsCar Mustang = new SportsCar("Ford", 2019, "red/black", true);
+		Truck F150 = new Truck("Ford", 2016, "blue", true);
 		System.out.println(Mustang);
+		SportsCar BMW3 = new SportsCar("BMW", 2007, "White", true);
 		Mustang.startEngine(true);
 		Mustang.revEngine();
 		Mustang.setCruiseControl(75);
+		SportsCar GT = new SportsCar("Ford", 2006, "blue/white", true);
 		F150.startEngine();
 		F150.fillGas();
 		Mustang.fillGas("gradeA");
 		Mustang.turningOnHandFreeset();
 		Mustang.makeCall("1234567892");
-		MiniVan Sienna = new MiniVan("Ford", 2000, "green", false);
+		Truck Tundra = new Truck("Toyota", 1993, "blue", false);
+		MiniVan Sienna = new MiniVan("Ford", 2002, "green", false);
 		Sienna.turningOnHandFreeset();
 		Sienna.fillGas("gradeB");
 		Mustang.autoDrive();
-		Truck F350 = new Truck("Ford",2019,"black",true);
+		Truck F350 = new Truck("Ford",2018,"black",true);
 		F350.startEngine();
-		System.out.println(Automobile.autoList);
+		System.out.println("-----------------Now Lets take a look at our unsorted Array List------------------");
+		for(Automobile objectinAutomobileList: Automobile.autoList) {
+			System.out.println(objectinAutomobileList);
+		}
+		System.out.println("---------------------Now are Sorted ArrayList by Year in descending order---------");
+		Collections.sort(Automobile.autoList, Automobile.yearComparator);
+		for(Automobile objectinAutomobileList: Automobile.autoList) {
+			System.out.println(objectinAutomobileList);
+		}
+		
 	}
 }
